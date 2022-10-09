89507357091a1661907bcdd4d3cc26a3947dee62
unknown <nsinifranc@gmail.com>
2022-10-09 17:54:52 +0100

    chages

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,14 +9,26 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   result: any;
+  error: any;
 
   constructor(private http: HttpClient) { }
 
   loginUser(users: {email: string, password: string, status: string}){
     console.log(users)
     this.http.post("http://50-116-1-120.ip.linodeusercontent.com/teachers/login/", users).subscribe(
-      data=>this.result=data
+      data=>this.result=data,
+      error => this.error=error.error.detail
     )
+  }
+
+  getUser(users: {email: string, password: string, status: string}){
+    
+    if (users.status == '2'){
+      this.loginUser(users)
+    }else{
+      console.log(users.status)
+    }
+    
   }
 
   ngOnInit(): void {
